f3a136b28d5087cc7886228c49166b378250940e
Kalyan Mondal <kalyan.x.mondal@gmail.com>
2022-12-08 18:28:42 -0500

    game

---
+++ script.js
@@ -0,0 +1,46 @@
+player = document.getElementById("player");
+const initialYValue = 300;
+const initalXValue = 0;
+var pos = 0;
+
+const gravity = () =>{
+    frame = setInterval(eachFrame, 50);
+    function eachFrame(){
+        if (pos == 0){
+            clearInterval(frame);
+            player.style.left = '10px'; 
+            player.style.top = '350px';
+        }else{
+            pos--;
+            player.style.top = (parseInt(player.style.top) + pos)+'px';
+        }
+    }
+}
+
+const jump = () =>{
+    id = setInterval(perFrame, 50);
+    function perFrame(){
+        if(pos == 10){
+            gravity()
+            clearInterval(id);
+        }else{
+            pos++;
+            player.style.top = (parseInt(player.style.top) - pos)+'px';
+        }
+    }
+}
+
+window.addEventListener('load', ()=>{
+    
+    player.style.left = '10px'; 
+    player.style.top = '350px';
+    console.log("loaded")
+})
+document.addEventListener('keyup', (e) =>{
+    if(e.keyCode == "32"){
+        jump()
+    }
+
+
+    }
+)
